b6c2f156852d3fb032ec27ac2e36fe54b6962776
helloitsjoe <jboyle617@gmail.com>
2020-06-21 16:58:21 -0400

    Basic app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,60 @@
 import React from 'react';
+import { fetchUser } from './services';
+import './App.scss';
 
-export default function App() {
-  return 'Hi';
-}
+const IDLE = 'IDLE';
+const LOADING = 'LOADING';
+const SUCCESS = 'SUCCESS';
+const ERROR = 'ERROR';
+
+const initialState = {
+  status: IDLE,
+  errorMessage: '',
+  data: null,
+};
+
+const fetchReducer = (s = initialState, a) => {
+  const { type, errorMessage, data } = a;
+  return {
+    [LOADING]: { ...s, status: LOADING, data: null, errorMessage: '' },
+    [SUCCESS]: { ...s, status: SUCCESS, data: JSON.stringify(data, null, 4), errorMessage: '' },
+    [ERROR]: { ...s, status: ERROR, data: null, errorMessage },
+  }[type];
+};
+
+const App = () => {
+  const [category, setCategory] = React.useState('people');
+  const [search, setSearch] = React.useState('');
+  const [state, dispatch] = React.useReducer(fetchReducer, initialState);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch({ type: LOADING });
+    fetchUser({ category, search })
+      .then(data => {
+        dispatch({ type: SUCCESS, data });
+      })
+      .catch(err => {
+        dispatch({ type: ERROR, errorMessage: err.message });
+      });
+  };
+
+  return (
+    <form className="App-form" onSubmit={handleSubmit}>
+      <h1>Star Wars Search!</h1>
+      <select id="category" onChange={e => setCategory(e.target.value)}>
+        <option value="people">People</option>
+        <option value="films">Films</option>
+        <option value="planets">Planets</option>
+        <option value="species">Species</option>
+        <option value="starships">Starships</option>
+        <option value="vehicles">Vehicles</option>
+      </select>
+      <input placeholder="Search" onChange={e => setSearch(e.target.value)} />
+      <button type="submit">Submit</button>
+      <pre className="App-loading">{state.status === LOADING ? 'Loading...' : state.data}</pre>
+    </form>
+  );
+};
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,20 @@
+const path = require('path');
 const { makeWebpackConfig } = require('webpack-simple');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = makeWebpackConfig();
 
-module.exports = config;
+module.exports = {
+  ...config,
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.template.html',
+      filename: '../dist/index.html',
+    }),
+  ],
+};
