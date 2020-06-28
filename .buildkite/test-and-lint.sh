5b76227c7f0c18fbd975555b2342d08d4ecb9389
helloitsjoe <jboyle617@gmail.com>
2020-06-28 06:58:58 -0400

    coverage on master

--- .buildkite/test-and-lint.sh
+++ .buildkite/test-and-lint.sh
@@ -6,8 +6,10 @@ yarn lint
 
 echo '--- :jest: Testing!'
 yarn test --coverage
+
+echo '--- :coverage: Coveralls!'
 echo $COVERALLS_REPO_TOKEN
-COVERALLS_SERVICE_NAME=buildkite COVERALLS_GIT_BRANCH=coveralls yarn coveralls
+COVERALLS_SERVICE_NAME=buildkite COVERALLS_GIT_BRANCH=master yarn coveralls
 
 # tar -czf node_modules.tar.gz node_modules
 # echo '--- Artifacting!'
